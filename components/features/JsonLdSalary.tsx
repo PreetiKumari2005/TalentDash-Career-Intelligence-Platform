7f803f7c8a6587e5b993477e30974c2a84af5c06
honey-saxena <saxenahoney6598@gmail.com>
2026-06-19 11:41:30 +0530

    fix: fix str typo to string in JsonLdSalary

--- components/features/JsonLdSalary.tsx
+++ components/features/JsonLdSalary.tsx
@@ -1,37 +1,48 @@
-import * as React from "react";
-
 interface JsonLdSalaryProps {
-  companyName: str;
-  role: string;
-  baseSalary: number;
-  currency: string;
-  location: string;
+  companyName: string
+  role: string
+  baseSalary: number
+  currency: string
+  location: string
 }
 
-export const JsonLdSalary: React.FC<JsonLdSalaryProps> = ({ companyName, role, baseSalary, currency, location }) => {
-  const schema = {
-    "@context": "https://schema.org",
-    "@type": "Occupation",
-    "name": role,
-    "estimatedSalary": {
-      "@type": "MonetaryAmountDistribution",
-      "currency": currency,
-      "duration": "P1Y",
-      "median": {
-        "@type": "QuantitativeValue",
-        "value": baseSalary
-      }
+export function JsonLdSalary({
+  companyName,
+  role,
+  baseSalary,
+  currency,
+  location,
+}: JsonLdSalaryProps) {
+  const jsonLd = {
+    '@context': 'https://schema.org',
+    '@type': 'JobPosting',
+    title: role,
+    hiringOrganization: {
+      '@type': 'Organization',
+      name: companyName,
     },
-    "occupationLocation": {
-      "@type": "City",
-      "name": location
-    }
-  };
+    jobLocation: {
+      '@type': 'Place',
+      address: {
+        '@type': 'PostalAddress',
+        addressLocality: location,
+      },
+    },
+    baseSalary: {
+      '@type': 'MonetaryAmount',
+      currency: currency,
+      value: {
+        '@type': 'QuantitativeValue',
+        value: baseSalary,
+        unitText: 'YEAR',
+      },
+    },
+  }
 
   return (
     <script
       type="application/ld+json"
-      dangerouslySetInnerHTML={{ __html: JSON.stringify(schema) }}
+      dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
     />
-  );
-};
+  )
+}
